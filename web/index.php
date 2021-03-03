82f93f09c3dac9405fc640eb75cf1cc6fb56c823
Joby Elliott <code@byjoby.com>
2021-03-03 16:18:30 -0700

    v2 reorganization

--- web/index.php
+++ web/index.php
@@ -2,7 +2,12 @@
 /**
  * Set $SITE_PATH so it's available for setting up the CMS
  */
-$SITE_PATH = realpath(__DIR__.'/..');
+$SITE_PATH = realpath(__DIR__ . '/..');
+
+/**
+ * Almost all autoloading is handled by Composer
+ */
+require $SITE_PATH . '/vendor/autoload.php';
 
 /**
  * Set any additional paths. The preferred method for setting paths is in
@@ -27,24 +32,18 @@ $ERRORS = [];
 # Do not edit this code, it will be replaced whenever composer update/install runs
 # digraph-project-core/files/index-setup.php => web/index.php
 
-# almost all autoloading is handled by Composer
-require $SITE_PATH.'/digraph/vendor/autoload.php';
-
 # set up new config file to get started
 $config = new \Flatrr\Config\Config();
-
-# load project default config
-$config->readFile($SITE_PATH.'/digraph/core/digraph.yaml');
 
 # set site path
 $config['paths.site'] = $SITE_PATH;
 
 # load site config, overwriting anything else set
-$config->readFile($SITE_PATH.'/digraph.yaml', null, true);
+$config->readFile($SITE_PATH . '/digraph.yaml', null, true);
 
 # load environment config, overwriting anything else set
-if (file_exists($SITE_PATH.'/env.yaml')) {
-$config->readFile($SITE_PATH.'/env.yaml', null, true);
+if (file_exists($SITE_PATH . '/env.yaml')) {
+    $config->readFile($SITE_PATH . '/env.yaml', null, true);
 }
 
 # override config paths using array from index.php
@@ -52,11 +51,11 @@ $config->merge($PATHS, 'paths', true);
 
 # set cache path to system temp as a fallback, because we NEED a cache
 if (!$config['paths.cache'] || !is_writeable($config['paths.cache'])) {
-$WARNINGS[] = 'Cache directory is not set or not writeable. Falling back to path in sys_get_temp_dir()';
-$config['paths.cache'] = sys_get_temp_dir().'/digraph-cache';
-if (!is_writeable($config['paths.cache'])) {
-$ERRORS[] = 'Cache directory is not writeable. Site may not behave correctly.';
-}
+    $WARNINGS[] = 'Cache directory is not set or not writeable. Falling back to path in sys_get_temp_dir()';
+    $config['paths.cache'] = sys_get_temp_dir() . '/digraph-cache';
+    if (!is_writeable($config['paths.cache'])) {
+        $ERRORS[] = 'Cache directory is not writeable. Site may not behave correctly.';
+    }
 }
 
 # set up CMS using Bootstrapper
@@ -65,19 +64,20 @@ $ERRORS[] = 'Cache directory is not writeable. Site may not behave correctly.';
 $cms = \Digraph\Bootstrapper::bootstrap($config);
 
 # load site config, overwriting anything else set, done twice to override modules
-$config->readFile($SITE_PATH.'/digraph.yaml', null, true);
+$config->readFile($SITE_PATH . '/digraph.yaml', null, true);
 
 # load environment config, overwriting anything else set, done twice to override modules
-if (file_exists($SITE_PATH.'/env.yaml')) {
-$config->readFile($SITE_PATH.'/env.yaml', null, true);
+if (file_exists($SITE_PATH . '/env.yaml')) {
+    $config->readFile($SITE_PATH . '/env.yaml', null, true);
 }
 
 # set up new request/response package
 # it's advisable to use the Bootstrapper url() method for
 # getting your query string
 $package = new Digraph\Mungers\Package([
-'request.url' => \Digraph\Bootstrapper::url()
+    'request.url' => \Digraph\Bootstrapper::url(),
 ]);
+
 # END DIGRAPH-MANAGED: INDEX-SETUP
 
 /**
@@ -104,6 +104,7 @@ foreach ($ERRORS as $message) {
 # in the "fullmunge" config
 # by default this means building a response and also rendering it
 $cms->fullMunge($package);
+
 # END DIGRAPH-MANAGED: INDEX-EXECUTE
 
 /**
